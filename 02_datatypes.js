279e4ad3d55ca52b5bf1abe76483ec2e8d385bb8
Prince Upadhyay <upadhyayprince946@gmail.com>
2025-04-02 23:08:34 +0530

    Update 02_datatypes.js

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -8,8 +8,13 @@ let isloggedin = false;
 let value = null;
 let state;
 
+const id=Symbol('123');
+const anotherid=Symbol('123');
+
+console.log(id==anotherid);//false
+
 // data_types_in_js
-//1.premitive data type
+//1.premitive data type(7 types)
 // number => 2 to power of 53
 // bigint =>
 // string =>""
@@ -17,6 +22,21 @@ let state;
 // null => standalone value
 // undefined => value is not assigned yet
 // symbol => unique
+
+//2.reference (Non primitive)
+// array,objects,functions.
+
+//array
+const heros=["shaktiman","naagraj","doga"];
+//objects
+let myobj= {
+  name:"prince",
+  age:21,  
+}
+//function
+const myfun=function(){
+  console.log("hello world");
+}
 
 //object
 console.log(typeof ("prince"));
